660e7dff09c9b79d582799cf28b8e4913b721747
Seify <seify1@gmail.com>
2025-07-13 21:36:35 +0200

    Loading new users on scroll. Crash fix when perform search after user deletion

--- RandomUser/Views/UsersListView.swift
+++ RandomUser/Views/UsersListView.swift
@@ -10,24 +10,7 @@ struct UsersListView: View {
         sort: \RandomUserModel.timestamp
     ) private var users: [RandomUserModel]
 
-    private var filteredUsers: [RandomUserModel] {
-        users.filter { user in
-            if searchText.isEmpty {
-                return true
-            }
-
-            guard let token = selectedTokens.first else { return true }
-
-            return switch token {
-                case .name:
-                    user.firstName.localizedStandardContains(searchText)
-                case .surname:
-                    user.lastName.localizedStandardContains(searchText)
-                case .email:
-                    user.email.localizedStandardContains(searchText)
-            }
-        }
-    }
+    @State private var filteredUsers: [RandomUserModel] = []
 
     @State private var searchText: String = ""
     @State var selectedTokens = [UserSearchToken]()
@@ -42,6 +25,11 @@ struct UsersListView: View {
                     } label: {
                         UserCellView(user: user)
                     }
+                    .onAppear {
+                        Task {
+                            await loadUsersIfNeeded(lastAppearedUser: user)
+                        }
+                    }
                 }
                 .onDelete(perform: deleteUsers)
             }
@@ -54,19 +42,42 @@ struct UsersListView: View {
             suggestedTokens: $suggestedTokens,
             token: { Text($0.rawValue) }
         )
-        .onSubmit(of: .search) {
-            print("Submit!")
+        .onChange(of: searchText) {
+            // TODO: add debounce
+            // https://tarkalabs.com/blogs/debounce-in-swift/
+            updateFilteredUsers()
         }
-        .task {
-            do {
-                let users = try await RandomClient(decoder: RandomJsonDecoder()).getUsers()
-                for user in users.results {
-                    let model = RandomUserModel(user: user)
-                    modelContext.insert(model)
+        .onAppear() {
+            updateFilteredUsers()
+            Task {
+                if users.count == 0 {
+                    await loadUsers()
                 }
-            } catch {
-                print(error)
             }
+        }
+    }
+
+    private func loadUsersIfNeeded(lastAppearedUser: RandomUserModel) async {
+        guard searchText.isEmpty, selectedTokens.isEmpty else {
+            return
+        }
+
+        if let lastUser = users.last, lastUser.uuid == lastAppearedUser.uuid {
+            await loadUsers()
+        }
+    }
+
+    private func loadUsers() async {
+        do {
+            let users = try await RandomClient(decoder: RandomJsonDecoder()).getUsers()
+            for user in users.results {
+                let model = RandomUserModel(user: user)
+                modelContext.insert(model)
+            }
+            try modelContext.save()
+            updateFilteredUsers()
+        } catch {
+            print(error)
         }
     }
 
@@ -81,6 +92,26 @@ struct UsersListView: View {
                 } catch {
                     print("failed to save model")
                 }
+                updateFilteredUsers()
+            }
+        }
+    }
+
+    private func updateFilteredUsers() {
+        filteredUsers = users.filter { user in
+            if searchText.isEmpty {
+                return true
+            }
+
+            guard let token = selectedTokens.first else { return true }
+
+            return switch token {
+                case .name:
+                    user.firstName.localizedStandardContains(searchText)
+                case .surname:
+                    user.lastName.localizedStandardContains(searchText)
+                case .email:
+                    user.email.localizedStandardContains(searchText)
             }
         }
     }
